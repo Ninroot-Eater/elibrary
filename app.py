7c6c6923be78aaf4c39c1693096178e4c1039460
Ninroot <averyformalname@gmail.com>
2021-07-08 01:45:33 +0800

    database logic

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template
+import sqlite3 as sql
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "verysecretkey"
 
+
+
 @app.route("/")
 def home():
+    con = sql.connect("data.db")
+    cur = con.cursor()
+    x = cur.execute("SELECT * FROM book")
+    for i in x:
+        print(i)
     return render_template("index.html")
-    
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- data.py
+++ data.py
@@ -1,11 +1,21 @@
 import sqlite3 as sql
-
+import csv
 
 con = sql.connect("data.db")
 cur = con.cursor()
 
-def book_table():
-    x = cur.execute("SELECT * FROM book")
-    return x
+def from_csv():
+    with open("blist.csv","r",encoding="utf-8") as f:
+        x = csv.reader(f)
+        x = [i for i in x]
+        c = 3
+        for i in x:
+            i.append("None")
+            i.insert(0,c)
+            print(i)
+            cur.execute(f"INSERT INTO book VALUES {tuple(i)}")
+            con.commit()
+            print("added")
+            c+=1
 
 
